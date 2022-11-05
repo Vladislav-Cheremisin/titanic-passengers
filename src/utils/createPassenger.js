54c4b02f50cd71be52bae4138e130ce8896ce3f9
Vladislav Cheremisin <vladislav.cheremisin@artimpex.kz>
2022-11-05 12:48:27 +0300

    feat: add simple test example

--- src/utils/createPassenger.js
+++ src/utils/createPassenger.js
@@ -5,7 +5,7 @@ export const createPassenger = (options) => {
     <span class="card__ticket"><strong>Ticket:</strong> ${options.ticket ? options.ticket : `Unknown`}</span>
     <span class="card__gender"><strong>Gender:</strong> ${options.gender ? options.gender : `Unknown`}</span>
     <span class="card__status"><strong>Status:</strong> ${options.survived ? `SURVIVED` : `NOT SURVIVED`}</span>
-    <span class="card__age"><strong>Age:</strong> ${options.age ? Math.floor(options.age) : `Unknown`} y.o.</span>
+    <span class="card__age"><strong>Age:</strong> ${options.age ? Math.floor(options.age) + ` y.o.`: `Unknown`}</span>
     <span class="card__cabin"><strong>cabin:</strong> ${options.cabin ? options.cabin : `Unknown`}</span>
   </div>
   `;
