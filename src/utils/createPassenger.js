ac6bec823746f4afa4d2f0962cd4638d8a9320a2
Vladislav Cheremisin <vladislav.cheremisin@artimpex.kz>
2022-11-15 12:32:34 +0300

    refactor: make createPassenger function a little bit easier

--- src/utils/createPassenger.js
+++ src/utils/createPassenger.js
@@ -1,26 +1,28 @@
+const createInfoSpan = (className, header, content) => {
+  const infoSpan = document.createElement('span');
+  const infoHeader = document.createElement('strong');
+  const infoText = document.createElement('span');
+
+  infoHeader.innerText = header + ': ';
+  infoText.innerText = content;
+
+  infoSpan.appendChild(infoHeader);
+  infoSpan.appendChild(infoText);
+  infoSpan.classList.add(className);
+
+  return infoSpan;
+};
+
 export const createPassenger = (opt) => {
   const passenger = document.createElement('div');
-  const passengerName = document.createElement('span');
-  const passengerTicket = document.createElement('span');
-  const passengerGender = document.createElement('span');
-  const passengerStatus = document.createElement('span');
-  const passengerAge = document.createElement('span');
-  const passengerCabin = document.createElement('span');
+  const passengerName = createInfoSpan('card__name', 'Name', `${opt.name ? opt.name : `Unknown`}`);
+  const passengerTicket = createInfoSpan('card__ticket', 'Ticket', `${opt.ticket ? opt.ticket : `Unknown`}`);
+  const passengerGender = createInfoSpan('card__gender', 'Gender', `${opt.gender ? opt.gender : `Unknown`}`);
+  const passengerStatus = createInfoSpan('card__status', 'Status', `${opt.survived ? `SURVIVED` : `NOT SURVIVED`}`);
+  const passengerAge = createInfoSpan('card__age', 'Age', `${opt.age ? Math.floor(opt.age) + ` y.o.` : `Unknown`}`);
+  const passengerCabin = createInfoSpan('card__cabin', 'Cabin', `${opt.cabin ? opt.cabin : `Unknown`}`);
 
   passenger.classList.add('card');
-  passengerName.classList.add('card__name');
-  passengerTicket.classList.add('card__ticket');
-  passengerGender.classList.add('card__gender');
-  passengerStatus.classList.add('card__status');
-  passengerAge.classList.add('card__age');
-  passengerCabin.classList.add('card__cabin');
-
-  passengerName.innerHTML = `<strong>Name:</strong> ${opt.name ? opt.name : `Unknown`}`;
-  passengerTicket.innerHTML = `<strong>Ticket:</strong> ${opt.ticket ? opt.ticket : `Unknown`}`;
-  passengerGender.innerHTML = `<strong>Gender:</strong> ${opt.gender ? opt.gender : `Unknown`}`;
-  passengerStatus.innerHTML = `<strong>Status:</strong> ${opt.survived ? `SURVIVED` : `NOT SURVIVED`}`;
-  passengerAge.innerHTML = `<strong>Age:</strong> ${opt.age ? Math.floor(opt.age) + ` y.o.` : `Unknown`}`;
-  passengerCabin.innerHTML = `<strong>Cabin:</strong> ${opt.cabin ? opt.cabin : `Unknown`}`;
 
   passenger.appendChild(passengerName);
   passenger.appendChild(passengerTicket);
